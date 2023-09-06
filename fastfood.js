615e2a2bcb17c497c8e646d5c444f8decf6c1442
Vraetzught <klaasbrems@gmail.com>
2023-09-06 15:36:32 +0200

    updated fastfood.html & fastfood.js

--- fastfood.js
+++ fastfood.js
@@ -10,11 +10,27 @@ const aantalDesserts = document.getElementsByName("dessert").length;
 document.getElementById("aantalDesserts").innerText = aantalDesserts;
 
 document.getElementById("hamburgers").onchange = function() {
-   document.getElementById("keuzeBurger").innerText = this.value;
+   const gekozenNummer = this.value;
+   const gekozenNaam = this.options[this.selectedIndex].innerText;
+   document.getElementById("keuzeBurger").innerText = `${gekozenNummer}: ${gekozenNaam}`;
 }
 
 for (const inputFriet of document.getElementsByName("friet")) {
    inputFriet.onchange = function() {
-      document.getElementById("keuzeFriet").innerText = this.value;
-   };
+      const gekozenNummer = this.value;
+      const gekozenNaam = this.parentElement.innerText;
+      document.getElementById("keuzeFriet").innerText = `${gekozenNummer}: ${gekozenNaam}`;
+   }
+}
+
+for (const inputDessert of document.getElementsByName("dessert")) {
+   inputDessert.onchange = function() {
+      let keuzes = "";
+      for (const eenInputDessert of document.getElementsByName("dessert")) {
+         if (eenInputDessert.checked) {
+            keuzes += `${eenInputDessert.value}: ${eenInputDessert.parentElement.innerText}, `;
+         }
+      }
+      document.getElementById("keuzeDessert").innerText = keuzes.slice(0, -1);
+   }
 }
